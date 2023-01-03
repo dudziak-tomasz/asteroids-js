1da9a7a29809490e32cb57bb048b484b18f9bca2
Tomasz Dudziak <dudziak.tomasz@gmail.com>
2023-01-03 11:16:05 +0100

    test: add tests for src/user.js in tests/src.user.test.js

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,7 @@ import { db } from '../src/db/db.js'
 import { configTest } from './src.config.test.js'
 import { dbTest } from './src.db.db.test.js'
 import { chatServerTest } from './src.chatserver.test.js'
+import { userTest } from './src.user.test.js'
 
 
 // config.js should be tested first, because is used by db.js
@@ -12,6 +13,7 @@ db.connect()
 
 await dbTest()
 await chatServerTest()
+await userTest()
 
 db.disconnect()
 
