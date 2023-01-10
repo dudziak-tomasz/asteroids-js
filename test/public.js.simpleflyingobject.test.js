bb1fb267991f5b7404ba244159dec5e305922a55
Tomasz Dudziak <dudziak.tomasz@gmail.com>
2023-01-10 14:43:57 +0100

    test: add tests for public/js/simpleflyingobject.js in test/public.js.simpleflyingobject.test.js

---
+++ test/public.js.simpleflyingobject.test.js
@@ -0,0 +1,31 @@
+import assert from 'assert'
+import { test } from 'node:test'
+import { Spacetime } from '../public/js/spacetime.js'
+import { SimpleFlyingObject } from '../public/js/simpleflyingobject.js'
+
+import './__mocks__/mock.dom.js'
+import './__mocks__/mock.spacetime.js'
+
+
+test('Should create simpleFlyingObject and assign data', () => {
+    const simpleFO = new SimpleFlyingObject()
+    assert.deepEqual(simpleFO.width, 3)
+    assert.deepEqual(simpleFO.height, 3)
+    assert.deepEqual(simpleFO.canvas.constructor.name, 'HTMLDivElement')
+    assert.deepEqual(simpleFO.canvas.style.backgroundColor, 'white')
+    assert.deepEqual(simpleFO.canvas.style.position, 'absolute')
+    assert.deepEqual(simpleFO.canvas.style.width, '3px')
+    assert.deepEqual(simpleFO.canvas.style.height, '3px')
+})
+
+
+test('Should change style after .move()', () => {
+    const simpleFO = new SimpleFlyingObject()
+    simpleFO.left = 10
+    simpleFO.top = 10
+    simpleFO.speedX = 2
+    simpleFO.speedY = -3
+    simpleFO.move()
+    assert.deepEqual(simpleFO.canvas.style.left, '12px')
+    assert.deepEqual(simpleFO.canvas.style.top, '7px')
+})
