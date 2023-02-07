3d092eefcc5eee4fae7b42e5835cd19ba32ecfcd
Tomasz Dudziak <dudziak.tomasz@gmail.com>
2023-02-07 10:26:54 +0100

    test: add extra tests for touch events in controls
    in test/public.js.controls.js

--- test/public.js.controls.js
+++ test/public.js.controls.js
@@ -19,10 +19,13 @@ test('Should assign default data', () => {
     assert.deepEqual(controls.fire, [])
     assert.deepEqual(controls.hyperspace, [])
     assert.deepEqual(controls.isTouch, false)
+    assert.deepEqual(controls.touchMoveSensitivity, 30)
+    assert.deepEqual(controls.touchAccelerationSensitivity, 50)
+    assert.deepEqual(controls.touchHyperspaceSensitivity, 100)
 })
 
 
-test('Should add event listeners', () => {
+test('Should add mouse and keyboard event listeners', () => {
     // Prepare mocks
     let checkMock
     game.checkControls = () =>  checkMock = true
@@ -267,3 +270,264 @@ test('Should remove mouse from hyperspace', () => {
     controls.eventMouseUp(event)
     assert.deepEqual(controls.hyperspace.length, 0)
 })
+
+
+test('Should set isTouch to false', () => {
+    controls.isTouch = true
+    controls.eventMouseUp()
+    assert.deepEqual(controls.isTouch, false)
+})
+
+
+test('Should set isTouch, touchStart after touchstart event', () => {
+    // Prepare mocks
+    const event = {
+        targetTouches: [
+            {
+                clientX: 10,
+                clientY: 10
+            }
+        ]
+    }
+
+    controls.eventTouchStart(event)
+    assert.deepEqual(controls.isTouch, true)
+    assert.deepEqual(controls.touchStart[0], {x: 10, y: 10})
+})
+
+
+test('Should add touch to fire and set rotation to []', () => {
+    // Prepare mocks
+    const event = {
+        targetTouches: [
+            {
+                clientX: 10,
+                clientY: 10
+            }
+        ]
+    }
+
+    controls.fire = []
+    controls.rotation = ['left']
+    controls.eventTouchStart(event)
+    assert.deepEqual(controls.fire.length, 1)
+    assert.deepEqual(controls.rotation, [])
+})
+
+
+test('Should not add touch to fire and not set rotation to []', () => {
+    // Prepare mocks
+    const event = {
+        targetTouches: []
+    }
+
+    controls.fire = []
+    controls.rotation = ['left']
+    controls.eventTouchStart(event)
+    assert.deepEqual(controls.fire.length, 0)
+    assert.notDeepEqual(controls.rotation, [])
+})
+
+
+test('Should set touchEnd after touchmove event', () => {
+    // Prepare mocks
+    const event1 = {
+        targetTouches: [
+            {
+                clientX: 10,
+                clientY: 10
+            }
+        ]
+    }
+    const event2 = {
+        targetTouches: [
+            {
+                clientX: 20,
+                clientY: 20
+            }
+        ]
+    }
+
+    controls.eventTouchStart(event1)
+    controls.eventTouchMove(event2)
+    assert.deepEqual(controls.touchEnd[0], {x: 20, y: 20})
+})
+
+
+test('Should add left to rotation after touchmove event', () => {
+    // Prepare mocks
+    const event1 = {
+        targetTouches: [
+            {
+                clientX: 100,
+                clientY: 100
+            }
+        ]
+    }
+    const event2 = {
+        targetTouches: [
+            {
+                clientX: 69,
+                clientY: 100
+            }
+        ]
+    }
+
+    controls.rotation = []
+    controls.eventTouchStart(event1)
+    controls.eventTouchMove(event2)
+    assert.deepEqual(controls.rotation[0], 'left')
+})
+
+
+test('Should add right to rotation after touchmove event', () => {
+    // Prepare mocks
+    const event1 = {
+        targetTouches: [
+            {
+                clientX: 100,
+                clientY: 100
+            }
+        ]
+    }
+    const event2 = {
+        targetTouches: [
+            {
+                clientX: 131,
+                clientY: 100
+            }
+        ]
+    }
+
+    controls.rotation = []
+    controls.eventTouchStart(event1)
+    controls.eventTouchMove(event2)
+    assert.deepEqual(controls.rotation[0], 'right')
+})
+
+
+test('Should set accelerate after touchmove event', () => {
+    // Prepare mocks
+    const event1 = {
+        targetTouches: [
+            {
+                clientX: 100,
+                clientY: 100
+            }
+        ]
+    }
+    const event2 = {
+        targetTouches: [
+            {
+                clientX: 100,
+                clientY: 49
+            }
+        ]
+    }
+
+    controls.accelerate = false
+    controls.eventTouchStart(event1)
+    controls.eventTouchMove(event2)
+    assert.deepEqual(controls.accelerate, true)
+})
+
+
+test('Should add touch to hyperspace after touchmove event', () => {
+    // Prepare mocks
+    const event1 = {
+        targetTouches: [
+            {
+                clientX: 100,
+                clientY: 100
+            }
+        ]
+    }
+    const event2 = {
+        targetTouches: [
+            {
+                clientX: 100,
+                clientY: 201
+            }
+        ]
+    }
+
+    controls.hyperspace = []
+    controls.eventTouchStart(event1)
+    controls.eventTouchMove(event2)
+    assert.deepEqual(controls.hyperspace[0], 'touch')
+})
+
+
+test('Should reset data after touchend event', () => {
+    controls.accelerate = true
+    controls.fire = ['touch']
+    controls.hyperspace = ['touch']
+
+    controls.eventTouchEnd()
+
+    assert.deepEqual(controls.accelerate, false)
+    assert.deepEqual(controls.fire, [])
+    assert.deepEqual(controls.hyperspace, [])
+})
+
+
+test('Should return true: isHyperspace()', () => {
+    controls.hyperspace = ['touch']
+    assert.deepEqual(controls.isHyperspace(), true)
+})
+
+
+test('Should return false: isHyperspace()', () => {
+    controls.hyperspace = []
+    assert.deepEqual(controls.isHyperspace(), false)
+})
+
+
+test('Should return true: isFire()', () => {
+    controls.fire = ['mouse']
+    assert.deepEqual(controls.isFire(), true)
+})
+
+
+test('Should return false: isFire()', () => {
+    controls.fire = []
+    assert.deepEqual(controls.isFire(), false)
+})
+
+
+test('Should return true: isRotation()', () => {
+    controls.rotation = ['right']
+    assert.deepEqual(controls.isRotation(), true)
+})
+
+
+test('Should return false: isRotation()', () => {
+    controls.rotation = []
+    assert.deepEqual(controls.isRotation(), false)
+})
+
+
+test('Should return right: rotationDirection()', () => {
+    controls.rotation = ['right', 'left']
+    assert.deepEqual(controls.rotationDirection(), 'right')
+})
+
+
+test('Should return left: rotationDirection()', () => {
+    controls.rotation = ['left', 'right']
+    assert.deepEqual(controls.rotationDirection(), 'left')
+})
+
+
+test('Should return true: isAccelerate()', () => {
+    controls.accelerate = true
+    assert.deepEqual(controls.isAccelerate(), true)
+})
+
+
+test('Should return false: isAccelerate()', () => {
+    controls.accelerate = false
+    assert.deepEqual(controls.isAccelerate(), false)
+})
+
+
