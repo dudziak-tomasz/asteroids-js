1bfd2a89391ca954185bc23748c7866c6e8a361e
Tomasz Dudziak <dudziak.tomasz@gmail.com>
2023-02-15 13:10:49 +0100

    test: add extra tests for public/js/game.js
    add tests for methods: generateSaucer, increaseProbabilityCreateSaucer,
    extraLife, switchPause, checkControls;
    add tests for custom event listeners
    in test/public.js.game.test.js

--- test/public.js.game.test.js
+++ test/public.js.game.test.js
@@ -8,8 +8,10 @@ import { Spacetime } from '../public/js/spacetime.js'
 import { api } from '../public/js/api.js'
 import { alert } from '../public/js/alert.js'
 import { lives } from '../public/js/lives.js'
+import { score } from '../public/js/score.js'
 import { leaderboard } from '../public/js/leaderboard.js'
 import { chat } from '../public/js/chat.js'
+import { controls } from '../public/js/controls.js'
 import { sleep } from './__mocks__/mock.sleep.js'
 import { game } from '../public/js/game.js'
 
@@ -274,8 +276,485 @@ test('Should call startSpaceship from PressFireIfNoSpaceship', () => {
 })
 
 
+test('Should not generate saucer if is not spaceship', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = () => saucerMock = true
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, false)
+})
 
 
-test('Should listen for custom events', {todo: true}, () => {
+test('Should not generate saucer if is saucer', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = () => saucerMock = true
+    Spacetime.spaceship = true
+    Spacetime.saucer = true
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, false)
+})
+
+
+test('Should not generate saucer if game paused', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = () => saucerMock = true
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+    game.pause = true
+    game.isLevelStarting = false
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, false)
+})
+
+
+test('Should not generate saucer if level is starting', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = () => saucerMock = true
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+    game.pause = false
+    game.isLevelStarting = true
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, false)
+})
+
+
+test('Should not generate saucer if probability is too small', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = () => saucerMock = true
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+    game.pause = false
+    game.isLevelStarting = false
+    game.probabilityCreateSaucer = -1
+    score.score = 3000
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, false)
+})
+
+
+test('Should not generate saucer if score is too small', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = () => saucerMock = true
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+    game.pause = false
+    game.isLevelStarting = false
+    game.probabilityCreateSaucer = 1
+    score.score = 1000
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, false)
+})
+
+
+test('Should generate large saucer', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = (size) => saucerMock = size
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+    game.pause = false
+    game.isLevelStarting = false
+    game.probabilityCreateSaucer = 1
+    score.score = 3000
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, 2)
+})
+
+
+test('Should generate random size saucer', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = (size) => saucerMock = size
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+    game.pause = false
+    game.isLevelStarting = false
+    game.probabilityCreateSaucer = 1
+    score.score = 11000
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock === 1 || saucerMock === 2, true)
+})
+
+
+test('Should generate small saucer', () => {
+    // Prepare mocks
+    let saucerMock = false
+    Spacetime.createSaucer = (size) => saucerMock = size
+    Spacetime.spaceship = true
+    Spacetime.saucer = false
+    game.pause = false
+    game.isLevelStarting = false
+    game.probabilityCreateSaucer = 1
+    score.score = 41000
+
+    game.generateSaucer()
+    assert.deepEqual(saucerMock, 1)
+})
+
+
+test('Should increase probabilityCreateSaucer', () => {
+    game.probabilityCreateSaucer = game.probabilityCreateSaucerInitial
+    game.increaseProbabilityCreateSaucer()
+
+    assert.deepEqual(game.probabilityCreateSaucer > game.probabilityCreateSaucerInitial, true)
+})
+
+
+test('Should add life, blink score, send chat.updateScore, increase probabilityCreateSaucer', () => {
+    // Prepare mocks
+    let livesAddMock = false
+    lives.add = () => livesAddMock = true
+    let scoreBlinkMock = false
+    score.blink = () => scoreBlinkMock = true
+    let chatUpdateScore = false
+    chat.updateScore = () => chatUpdateScore = true
+
+    game.probabilityCreateSaucer = game.probabilityCreateSaucerInitial
+    game.extraLife()
+
+    assert.deepEqual(livesAddMock, true)
+    assert.deepEqual(scoreBlinkMock, true)
+    assert.deepEqual(chatUpdateScore, true)
+    assert.deepEqual(game.probabilityCreateSaucer > game.probabilityCreateSaucerInitial, true)
+})
+
+
+test('Should not switch game.pause', () => {
+    // Prepare mocks
+    game.pause = false
+    Spacetime.spaceship = undefined
+
+    game.switchPause()
+    assert.deepEqual(game.pause, false)
+})
+
+
+test('Should switch game.pause from false to true', () => {
+    // Prepare mocks
+    game.pause = false
+    Spacetime.spaceship = true
+    game.playAudio()
+    let spacetimeStopMock = false
+    Spacetime.stop = () => spacetimeStopMock = true
+
+    assert.deepEqual(game.audio.isPlaying, true)
+    game.switchPause()
+    assert.deepEqual(game.pause, true)
+    assert.deepEqual(spacetimeStopMock, true)
+    assert.deepEqual(game.audio.isPlaying, false)
+})
+
+
+test('Should switch game.pause from true to false', () => {
+    // Prepare mocks
+    game.pause = true
+    Spacetime.spaceship = {
+        stopFire: () => 0,
+        stopRotation: () => 0,
+        stopAccelerate: () => 0
+    }
+    game.stopAudio()
+    game.openBoxes = 0
+    let spacetimeStartMock = false
+    Spacetime.start = () => spacetimeStartMock = true
+
+    assert.deepEqual(game.audio.isPlaying, false)
+    game.switchPause()
+    assert.deepEqual(game.pause, false)
+    assert.deepEqual(spacetimeStartMock, true)
+    assert.deepEqual(game.audio.isPlaying, true)
+})
+
+
+
+test('Should not switch game.pause from true to false when there are open boxes', () => {
+    // Prepare mocks
+    game.pause = true
+    Spacetime.spaceship = {
+        stopFire: () => 0,
+        stopRotation: () => 0,
+        stopAccelerate: () => 0
+    }
+    game.stopAudio()
+    game.openBoxes = 1
+    let spacetimeStartMock = false
+    Spacetime.start = () => spacetimeStartMock = true
+
+    assert.deepEqual(game.audio.isPlaying, false)
+    game.switchPause()
+    assert.deepEqual(game.pause, true)
+    assert.deepEqual(spacetimeStartMock, false)
+    assert.deepEqual(game.audio.isPlaying, false)
+})
+
+
+test('Should listen for spaceshiphit event', () => {
+    // Prepare mocks
+    let pressFireToPlayNewSpaceshipMock = false
+    game.pressFireToPlayNewSpaceship = () => pressFireToPlayNewSpaceshipMock = true
+  
+    game.mainDiv.dispatchEvent(new CustomEvent('spaceshiphit'))
+    assert.deepEqual(pressFireToPlayNewSpaceshipMock, true)
+})
+
+
+test('Should listen for noasteroids event', () => {
+    // Prepare mocks
+    let startLevelMock = false
+    game.startLevel = () => startLevelMock = true
+
+    game.mainDiv.dispatchEvent(new CustomEvent('noasteroids'))
+    assert.deepEqual(startLevelMock, true)
+})
+
+
+test('Should listen for onesecond event', () => {
+    // Prepare mocks
+    let generateSaucerMock = false
+    game.generateSaucer = () => generateSaucerMock = true
+
+    game.seconds = game.secondsBetweenSaucers - 1
+    game.mainDiv.dispatchEvent(new CustomEvent('onesecond'))
+    assert.deepEqual(game.seconds, game.secondsBetweenSaucers)
+    assert.deepEqual(generateSaucerMock, true)
+})
+
+
+test('Should listen for boxopen event', () => {
+    // Prepare mocks
+    let switchPauseMock = false
+    game.switchPause = () => switchPauseMock = true
+
+    game.openBoxes = 0
+    game.pause = false
+    Spacetime.spaceship = true
+    game.mainDiv.dispatchEvent(new CustomEvent('boxopen'))
+    assert.deepEqual(game.openBoxes, 1)
+    assert.deepEqual(switchPauseMock, true)
+})
+
+
+test('Should listen for boxclose event', () => {
+    // Prepare mocks
+    let switchPauseMock = false
+    game.switchPause = () => switchPauseMock = true
+
+    game.openBoxes = 1
+    game.pause = true
+    game.mainDiv.dispatchEvent(new CustomEvent('boxclose'))
+    assert.deepEqual(game.openBoxes, 0)
+    assert.deepEqual(switchPauseMock, true)
+})
+
+
+test('Should listen for username event', () => {
+    // Prepare mocks
+    let leaderboardRefreshMock = false
+    leaderboard.refresh = () => leaderboardRefreshMock = true
+
+    game.pressFireTo = 'startgame'
+    game.mainDiv.dispatchEvent(new CustomEvent('username'))
+    assert.deepEqual(leaderboardRefreshMock, true)
+})
+
+
+test('Should call pressFireIfNoSpaceship from checkControls', () => {
+    // Prepare mocks
+    let pressFireIfNoSpaceshipMock = false
+    game.pressFireIfNoSpaceship = () => pressFireIfNoSpaceshipMock = true
+    game.openBoxes = 0
+    game.pause = false
+    Spacetime.spaceship = false
+    controls.fire = ['keyboard']
+
+    game.checkControls()
+    assert.deepEqual(pressFireIfNoSpaceshipMock, true)
 
 })
+
+
+test('Should return from checkControls if there are open boxes', () => {
+    // Prepare mocks
+    let pressFireIfNoSpaceshipMock = false
+    game.pressFireIfNoSpaceship = () => pressFireIfNoSpaceshipMock = true
+    game.openBoxes = 1
+    game.pause = false
+    Spacetime.spaceship = false
+    controls.fire = ['keyboard']
+
+    game.checkControls()
+    assert.deepEqual(pressFireIfNoSpaceshipMock, false)
+})
+
+
+test('Should return from checkControls if game is paused', () => {
+    // Prepare mocks
+    let pressFireIfNoSpaceshipMock = false
+    game.pressFireIfNoSpaceship = () => pressFireIfNoSpaceshipMock = true
+    game.openBoxes = 0
+    game.pause = true
+    Spacetime.spaceship = false
+    controls.fire = ['keyboard']
+
+    game.checkControls()
+    assert.deepEqual(pressFireIfNoSpaceshipMock, false)
+})
+
+
+test('Should call Spacetime.spaceship.startHyperspace from checkControls', () => {
+    // Prepare mocks
+    game.openBoxes = 0
+    game.pause = false
+    controls.hyperspace = ['keyboard']
+    controls.fire = []
+    controls.rotation = []
+    controls.accelerate = false
+    let startMock = false
+    Spacetime.spaceship = {
+        startHyperspace: () => startMock = true,
+        stopFire: () => 0,
+        stopRotation: () => 0,
+        stopAccelerate: () => 0,
+    }
+
+    game.checkControls()
+    assert.deepEqual(startMock, true)
+})
+
+
+test('Should call Spacetime.spaceship.startFire from checkControls', () => {
+    // Prepare mocks
+    game.openBoxes = 0
+    game.pause = false
+    controls.hyperspace = []
+    controls.fire = ['keyboard']
+    controls.rotation = []
+    controls.accelerate = false
+    let startMock = false
+    Spacetime.spaceship = {
+        startFire: () => startMock = true,
+        stopRotation: () => 0,
+        stopAccelerate: () => 0,
+    }
+
+    game.checkControls()
+    assert.deepEqual(startMock, true)
+})
+
+
+test('Should call Spacetime.spaceship.startRotation from checkControls', () => {
+    // Prepare mocks
+    game.openBoxes = 0
+    game.pause = false
+    controls.hyperspace = []
+    controls.fire = []
+    controls.rotation = ['left']
+    controls.accelerate = false
+    let startMock = false
+    Spacetime.spaceship = {
+        stopFire: () => 0,
+        startRotation: (direction) => startMock = direction,
+        stopAccelerate: () => 0,
+    }
+
+    game.checkControls()
+    assert.deepEqual(startMock, 'left')
+})
+
+
+test('Should call Spacetime.spaceship.startAccelerate from checkControls', () => {
+    // Prepare mocks
+    game.openBoxes = 0
+    game.pause = false
+    controls.hyperspace = []
+    controls.fire = []
+    controls.rotation = []
+    controls.accelerate = true
+    let startMock = false
+    Spacetime.spaceship = {
+        stopFire: () => 0,
+        stopRotation: () => 0,
+        startAccelerate: () => startMock = true,
+    }
+
+    game.checkControls()
+    assert.deepEqual(startMock, true)
+})
+
+
+test('Should call Spacetime.spaceship.stopFire from checkControls', () => {
+    // Prepare mocks
+    game.openBoxes = 0
+    game.pause = false
+    controls.hyperspace = []
+    controls.fire = []
+    controls.rotation = []
+    controls.accelerate = false
+    let stopMock = false
+    Spacetime.spaceship = {
+        stopFire: () => stopMock = true,
+        stopRotation: () => 0,
+        stopAccelerate: () => 0,
+    }
+
+    game.checkControls()
+    assert.deepEqual(stopMock, true)
+})
+
+
+test('Should call Spacetime.spaceship.stopRotation from checkControls', () => {
+    // Prepare mocks
+    game.openBoxes = 0
+    game.pause = false
+    controls.hyperspace = []
+    controls.fire = []
+    controls.rotation = []
+    controls.accelerate = false
+    let stopMock = false
+    Spacetime.spaceship = {
+        stopFire: () => 0,
+        stopRotation: () => stopMock = true,
+        stopAccelerate: () => 0,
+    }
+
+    game.checkControls()
+    assert.deepEqual(stopMock, true)
+})
+
+
+test('Should call Spacetime.spaceship.stopAccelerate from checkControls', () => {
+    // Prepare mocks
+    game.openBoxes = 0
+    game.pause = false
+    controls.hyperspace = []
+    controls.fire = []
+    controls.rotation = []
+    controls.accelerate = false
+    let stopMock = false
+    Spacetime.spaceship = {
+        stopFire: () => 0,
+        stopRotation: () => 0,
+        stopAccelerate: () => stopMock = true,
+    }
+
+    game.checkControls()
+    assert.deepEqual(stopMock, true)
+})
+
+
